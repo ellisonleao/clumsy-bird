aec20680c73fcc76c0ed074765d10672cdfd2162
Ellison Leão <ellisonleao@gmail.com>
2014-02-07 15:49:04 -0200

    Smooooth physics

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -8,34 +8,38 @@ var BirdEntity = me.ObjectEntity.extend({
     this.parent(x, y, settings);
     this.alwaysUpdate = true;
     this.gravity = 32;
-    this.pushForce = 1.1;
     this.gravityForce = 5;
-    this.maxAngleRotation = Number.prototype.degToRad(45);
+    this.maxAngleRotation = Number.prototype.degToRad(30);
+    this.maxAngleRotationDown = Number.prototype.degToRad(90);
     this.renderable.addAnimation("flying", [0, 1, 2]);
     this.renderable.addAnimation("idle", [0]);
     this.renderable.setCurrentAnimation("flying");
     this.animationController = 0;
+    this.animationVelocity = 200;
   },
 
   update: function(x, y){
     // mechanics
     if (me.input.isKeyPressed('fly')){
-      this.pos.add(new me.Vector2d(0, -this.gravity * me.timer.tick * this.pushForce));
+      var currentPos = this.pos.y;
+      tween = new me.Tween(this.pos).to({y: currentPos - 45}, 150);
+      tween.easing(me.Tween.Easing.Bounce.Out);
+      tween.start();
       this.gravityForce = 5;
       this.renderable.angle -= Number.prototype.degToRad(10) * me.timer.tick;
       if (this.renderable.angle > -this.maxAngleRotation)
         this.renderable.angle = -this.maxAngleRotation;
     }else{
-      this.gravityForce += 0.1;
+      this.gravityForce += 0.03;
       this.pos.add(new me.Vector2d(0, me.timer.tick * this.gravityForce));
-      this.renderable.angle += Number.prototype.degToRad(5) * me.timer.tick;
-      if (this.renderable.angle > this.maxAngleRotation)
-        this.renderable.angle = this.maxAngleRotation;
+      this.renderable.angle += Number.prototype.degToRad(6 * me.timer.tick);
+      if (this.renderable.angle > this.maxAngleRotationDown)
+        this.renderable.angle = this.maxAngleRotationDown;
     }
 
     //manual animation
     var actual = this.renderable.getCurrentAnimationFrame();
-    if (this.animationController++ % 100){
+    if (this.animationController++ % this.animationVelocity){
       actual++;
       this.renderable.setAnimationFrame(actual);
     }
@@ -71,7 +75,7 @@ var PipeEntity = me.ObjectEntity.extend({
   update: function(){
     // mechanics
     this.pos.add(new me.Vector2d(-this.gravity * me.timer.tick, 0));
-    if (this.pos.x < -88) {
+    if (this.pos.x < -this.renderable.spritewidth) {
       me.game.remove(this);
     }
     return true;
