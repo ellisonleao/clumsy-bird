d3287e29131b1fbc1c07bf41d797e744dbcf95eb
Ellison Leão <ellisonleao@gmail.com>
2014-02-11 19:03:59 -0200

    Fix score text position on game over screen

--- js/screens/gameover.js
+++ js/screens/gameover.js
@@ -26,22 +26,64 @@ game.GameOverScreen = me.ScreenObject.extend({
       gImageBoard
     ), 10);
 
+
+    var shareImg = me.loader.getImage('share');
+    var Start = me.Renderable.extend({
+      init: function(image, action, y){
+        this.image = me.loader.getImage(image);
+        this.image_hover = me.loader.getImage(image + '_hover');
+        this.action = action;
+        this.pos = new me.Vector2d(
+          me.video.getWidth()/ 2 - this.image.width/2,
+          y
+        );
+        this.parent(this.pos, this.image.width, this.image.height);
+        me.input.registerPointerEvent("mousedown", this, this.clicked.bind(this));
+      },
+
+      clicked: function(){
+        me.state.change(this.action);
+      },
+
+      draw: function(context){
+        if (this.containsPointV(me.input.mouse.pos)){
+          context.drawImage(this.image_hover, this.pos.x, this.pos.y);
+        }else{
+          context.drawImage(this.image, this.pos.x, this.pos.y);
+        }
+
+      },
+
+      update: function(){
+        return true;
+      },
+
+      onDestroyEvent: function(){
+          me.input.releasePointerEvent("mousedown", this);
+      }
+
+    });
+
     // add the dialog witht he game information
     me.game.world.addChild(new (me.Renderable.extend ({
         // constructor
         init : function() {
-            // size does not matter, it's just to avoid having a zero size renderable
+            // size does not matter, it's just to avoid having a zero size 
+            // renderable
             this.parent(new me.Vector2d(), 100, 100);
-            this.font = new me.Font('Arial Black', 50, 'black', 'center');
-            this.score = 'Final Score: ' + game.data.score;
-            this.timer = 'Steps: ' + Math.round(game.data.timer);
+            this.font = new me.Font('Arial Black', 40, 'black', 'left');
+            this.score = 'Final Score: ' + game.data.score.toString();
+            this.timer = 'Steps: ' + Math.round(game.data.timer).toString();
         },
         update : function () {
             return true;
         },
         draw : function (context) {
-            this.font.draw(context, this.score,  me.game.viewport.width/2,  me.game.viewport.height/2);
-            this.font.draw(context, this.timer,  me.game.viewport.width/2,  me.game.viewport.height/2 + 60);
+            this.stepsText = this.font.measureText(context, this.timer);
+            this.scoreText = this.font.measureText(context, this.score);
+            console.log(this.stepsText, this.scoreText);
+            this.font.draw(context, this.score,  me.game.viewport.width/2 - this.scoreText.width/2,  me.game.viewport.height/2);
+            this.font.draw(context, this.timer,  me.game.viewport.width/2 - this.stepsText.width/2,  me.game.viewport.height/2 + 60);
         }
     })), 12);
 
