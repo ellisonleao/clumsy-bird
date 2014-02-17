fe0edfbb4842c30cc84bbba85bb39467ddffb65f
Ellison Leão <ellisonleao@gmail.com>
2014-02-17 01:21:02 -0300

    Add fadeout on game start

--- js/screens/play.js
+++ js/screens/play.js
@@ -29,7 +29,7 @@ game.PlayScreen = me.ScreenObject.extend({
     );
     me.game.world.addChild(this.getReady, 11);
 
-    var popOut = new me.Tween(this.getReady.pos).to({y: -132}, 2000)
+    var fadeOut = new me.Tween(this.getReady).to({alpha: 0}, 2000)
       .easing(me.Tween.Easing.Linear.None)
       .onComplete(function(){
             game.data.start = true;
