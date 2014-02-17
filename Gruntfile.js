e5b0570c09e61d7d3654ee525b17e645afd74c22
Ellison Leão <ellisonleao@gmail.com>
2014-02-17 01:50:50 -0300

    add Gruntfile and now using minified version of the game on index page

---
+++ Gruntfile.js
@@ -0,0 +1,71 @@
+/*global module:false*/
+module.exports = function(grunt) {
+    var sourceFiles = [
+        'js/game.js',
+        'js/resources.js',
+        'js/entities/entities.js',
+        'js/entities/HUD.js',
+        'js/screens/title.js',
+        'js/screens/play.js',
+        'js/screens/gameover.js',
+    ];
+
+    // Project configuration.
+    grunt.initConfig({
+        concat: {
+            dist: {
+                src: sourceFiles,
+                dest: 'build/clumsy.js'
+            }
+        },
+
+        uglify: {
+            options: {
+                report: 'min',
+                preserveComments: 'some'
+            },
+            dist: {
+                files: {
+                    'build/clumsy-min.js': [
+                      sourceFiles
+                    ]
+                }
+            }
+        },
+
+        jshint: {
+            options: {
+                jshintrc: ".jshintrc"
+            },
+
+            beforeConcat: {
+                files: {
+                    src: sourceFiles
+                }
+            },
+
+            afterConcat: {
+                files: {
+                    src: [ sourceFiles ]
+                }
+            }
+        },
+
+        clean: {
+            dist: [
+                'build/clumsy.js',
+                'build/clumsy-min.js'
+            ],
+        },
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+
+    // Default task.
+    grunt.registerTask('default', ['concat', 'uglify']);
+    grunt.registerTask('lint', ['jshint:beforeConcat', 'concat', 'jshint:afterConcat']);
+};
