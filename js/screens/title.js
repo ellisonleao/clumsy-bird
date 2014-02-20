d2eba9b1bb50a735cc899db1c87970c4c4b7e966
shinriyo <shinriyo@gmail.com>
2014-02-20 20:33:28 +0900

    formatting
    good my format

--- js/screens/title.js
+++ js/screens/title.js
@@ -3,20 +3,20 @@ game.TitleScreen = me.ScreenObject.extend({
     this.font = null;    
   },
 
-	onResetEvent: function() {
+  onResetEvent: function() {
     me.audio.stop("theme");
     game.data.newHiScore = false;
     me.game.world.addChild(new BackgroundLayer('bg', 1));
 
-		me.input.bindKey(me.input.KEY.ENTER, "enter", true);
+    me.input.bindKey(me.input.KEY.ENTER, "enter", true);
         me.input.bindKey(me.input.KEY.SPACE, "enter", true);
-		me.input.bindMouse(me.input.mouse.LEFT, me.input.KEY.ENTER);
+    me.input.bindMouse(me.input.mouse.LEFT, me.input.KEY.ENTER);
 
     this.handler = me.event.subscribe(me.event.KEYDOWN, function (action, keyCode, edge) {
-			if (action === "enter") {
-			  me.state.change(me.state.PLAY);
-			}
-		});
+      if (action === "enter") {
+        me.state.change(me.state.PLAY);
+      }
+    });
 
     //logo
     var logoImg = me.loader.getImage('logo');
@@ -35,7 +35,7 @@ game.TitleScreen = me.ScreenObject.extend({
 
     me.game.world.addChild(new (me.Renderable.extend ({
         // constructor
-        init : function() {
+        init: function() {
             // size does not matter, it's just to avoid having a zero size 
             // renderable
             this.parent(new me.Vector2d(), 100, 100);
@@ -43,25 +43,22 @@ game.TitleScreen = me.ScreenObject.extend({
             this.text = me.device.touch ? 'Tap to start' : 'PRESS SPACE OR CLICK LEFT MOUSE BUTTON TO START';
             this.font = new me.Font('gamefont', 20, '#000');
         },
-        update : function () {
+        update: function () {
             return true;
         },
-        draw : function (context) {
+        draw: function (context) {
             var measure = this.font.measureText(context, this.text);
-            this.font.draw(context, this.text,  me.game.viewport.width/2 - measure.width/2,  me.game.viewport.height/2 + 50);
+            this.font.draw(context, this.text, me.game.viewport.width/2 - measure.width/2, me.game.viewport.height/2 + 50);
         }
     })), 12);
+  },
 
-
-	},
-
-	onDestroyEvent : function() {
-    	// unregister the event
-		me.event.unsubscribe(this.handler);
-		me.input.unbindKey(me.input.KEY.ENTER);
+  onDestroyEvent: function() {
+      // unregister the event
+    me.event.unsubscribe(this.handler);
+    me.input.unbindKey(me.input.KEY.ENTER);
         me.input.unbindKey(me.input.KEY.SPACE);
-		me.input.unbindMouse(me.input.mouse.LEFT);
+    me.input.unbindMouse(me.input.mouse.LEFT);
     me.game.world.removeChild(this.ground);
-	}
-
+  }
 });
