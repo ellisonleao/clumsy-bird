22046f6509bdcd75302fceffc46861a821d86f7d
yairwein <yairwein@gmail.com>
2018-03-31 13:02:22 +0300

    add alooma tracking

--- js/game.js
+++ js/game.js
@@ -43,12 +43,15 @@ var game = {
     },
 
     "loaded": function() {
+        alooma.track('init', {});
         me.state.set(me.state.MENU, new game.TitleScreen());
         me.state.set(me.state.PLAY, new game.PlayScreen());
         me.state.set(me.state.GAME_OVER, new game.GameOverScreen());
 
         me.input.bindKey(me.input.KEY.SPACE, "fly", true);
         me.input.bindKey(me.input.KEY.M, "mute", true);
+        me.input.bindKey(me.input.KEY.U, "multiplyUsers", true);
+        me.input.bindKey(me.input.KEY.G, "godMode", true);
         me.input.bindPointer(me.input.KEY.SPACE);
 
         me.pool.register("clumsy", game.BirdEntity);
